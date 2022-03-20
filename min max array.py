1465b574a330d5a737f72e5b2025a13e24377022
rakeshpanagatla <rakeshpanagatla@gmail.com>
2022-03-20 17:33:49 -0500

    arrays s30

---
+++ min max array.py
@@ -0,0 +1,3 @@
+#brute force : comparing each number with MIN and MAX i.e, 2n comparisons
+# efficient: comparing i with i+1 then the maximum from them is compared with MAX and minimum is compared with MIN. By this way we can make 3 comparisons instead of 4.
+# In both the above ways the overall TC is O(n)
---
+++ spiral matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        top, bottom, left, right = 0, len(matrix)-1, 0, len(matrix[0])-1
+        spiral_matrix = []
+        n = len(matrix)*len(matrix[0])
+
+        
+        while(top<=bottom and left<=right):
+            
+            for i in range(left, right+1):
+                spiral_matrix.append(matrix[top][i])
+            top += 1
+            
+            for i in range(top, bottom+1):
+                spiral_matrix.append(matrix[i][right])
+            right -= 1
+            
+            if(top<=bottom):
+                for i in range(right, left-1, -1):
+                    spiral_matrix.append(matrix[bottom][i])
+                bottom -= 1
+            
+            if(left<=right):
+                for i in range(bottom, top-1, -1):
+                    spiral_matrix.append(matrix[i][left])
+                left += 1
+       
+        return spiral_matrix
